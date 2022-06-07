d1e77cf07c8eb31a24c037398e7876ae1d4be15b
Your Name <you@example.com>
2022-06-08 00:44:44 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('config/connection.php');
+
+if (isset($_GET['controller'])) {
+    $controller = $_GET['controller'];
+    if (isset($_GET['action'])) {
+        $action = $_GET['action'];
+    } else {
+        $action = 'index';
+    }
+} else {
+    $controller = 'pages';
+    $action = 'home';
+}
+require_once('config/routes.php');
